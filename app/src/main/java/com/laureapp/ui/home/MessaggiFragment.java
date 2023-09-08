ab91841c6698e3ca016fafe43e51681d7e78c12e
Nicola Di Leva <83495055+Nickdl286@users.noreply.github.com>
2023-09-08 20:24:23 +0200

    Aggiornamento layout messaggistica

--- app/src/main/java/com/laureapp/ui/home/MessaggiFragment.java
+++ app/src/main/java/com/laureapp/ui/home/MessaggiFragment.java
@@ -7,8 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.laureapp.R;
+
+import java.util.ArrayList;
 
 public class MessaggiFragment extends Fragment {
 

d4b3c491b2acc1ae27b7e374840cafa11f1106d7
BarbieriGiuseppe <giuseppebarbieri16@gmail.com>
2023-10-11 18:29:35 +0200

    Cambiamenti: Realizzata la ricerca delle tesi in Elenco tesi
    To-Do
    Da creare la classifica delle tesi

--- app/src/main/java/com/laureapp/ui/card/TesiStudente/ElencoTesiFragment.java
+++ app/src/main/java/com/laureapp/ui/card/TesiStudente/ElencoTesiFragment.java
@@ -7,17 +7,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import androidx.appcompat.widget.SearchView;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.laureapp.R;
 import com.laureapp.ui.roomdb.entity.Tesi;
-
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -26,26 +26,24 @@ public class ElencoTesiFragment extends Fragment {
     Context context;
     private ElencoTesiAdapter adapter;
     private ListView listView;
-
+    private String titoloTesiCercata = "";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_elenco_tesi_studente,container,false);
-
-
-
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_elenco_tesi_studente, container, false);
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         context = getContext();
 
-        loadAllTesiData().addOnCompleteListener(tesiTask -> { //chiamo il metodo per ottenere le tesi in base alle id tesi ottenute
+        SearchView searchView = view.findViewById(R.id.searchTesiView);
+        listView = view.findViewById(R.id.listAllTesiView);
+
+        loadAllTesiData().addOnCompleteListener(tesiTask -> {
             if (tesiTask.isSuccessful()) {
                 ArrayList<Tesi> tesiList = tesiTask.getResult();
-
-                //mostro sulla listview tutte le tesi dello studente
-                listView = view.findViewById(R.id.listAllTesiView);
                 adapter = new ElencoTesiAdapter(getContext(), tesiList);
                 listView.setAdapter(adapter);
             } else {
@@ -53,6 +51,26 @@ public class ElencoTesiFragment extends Fragment {
             }
         });
 
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Handle search query submission (if needed)
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                titoloTesiCercata = newText;
+                loadSearchedTesiData(titoloTesiCercata).addOnCompleteListener(tesiTask -> {
+                    if (tesiTask.isSuccessful()) {
+                        ArrayList<Tesi> searchedTesiList = tesiTask.getResult();
+                    } else {
+                        Log.e("Tesi Firestore Error", "Error getting searched Tesi data", tesiTask.getException());
+                    }
+                });
+                return true;
+            }
+        });
     }
 
     private Task<ArrayList<Tesi>> loadAllTesiData() {
@@ -68,7 +86,6 @@ public class ElencoTesiFragment extends Fragment {
                             tesi.setId_tesi((Long) document.get("id_tesi"));
                             tesi.setId_vincolo((Long) document.get("id_vincolo"));
 
-                            //codice che serve per convertire la data da firebase a sql
                             com.google.firebase.Timestamp firebaseTimestamp = (com.google.firebase.Timestamp) document.get("data_pubblicazione");
                             Date javaDate = firebaseTimestamp.toDate();
                             java.sql.Timestamp sqlTimestamp = new java.sql.Timestamp(javaDate.getTime());
@@ -88,4 +105,50 @@ public class ElencoTesiFragment extends Fragment {
                 });
     }
 
+    /**
+     * Questo metodo serve per prendere ed aggiornare la lista delle tesi da firestore filtrandole
+     * in base al testo di ricerca
+     * @param searchText rappresenta il titolo della tesi da ricercare
+     * @return la lista delle tesi filtrata
+     */
+    private Task<ArrayList<Tesi>> loadSearchedTesiData(String searchText) {
+        final ArrayList<Tesi> tesiList = new ArrayList<>();
+
+        return FirebaseFirestore.getInstance()
+                .collection("Tesi") //collection di firebase
+                .orderBy("titolo") //ordino i risultati in base al titolo
+                .startAt(searchText)
+                .endAt(searchText + "\uf8ff")
+                .get()
+                .continueWith(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            //se la ricerca ha successo, assegno i campi della tesi in un oggetto Tesi
+                            Tesi tesi = new Tesi();
+                            tesi.setId_tesi((Long) document.get("id_tesi"));
+                            tesi.setId_vincolo((Long) document.get("id_vincolo"));
+
+                            // Converto da firebase timestamp a sql timestamp
+                            com.google.firebase.Timestamp firebaseTimestamp = (com.google.firebase.Timestamp) document.get("data_pubblicazione");
+                            Date javaDate = firebaseTimestamp.toDate();
+                            java.sql.Timestamp sqlTimestamp = new java.sql.Timestamp(javaDate.getTime());
+                            tesi.setData_pubblicazione(sqlTimestamp);
+
+                            tesi.setCiclo_cdl((String) document.get("ciclo_cdl"));
+                            tesi.setAbstract_tesi((String) document.get("abstract_tesi"));
+                            tesi.setTitolo((String) document.get("titolo"));
+                            tesi.setTipologia((String) document.get("tipologia"));
+
+                            tesiList.add(tesi);
+                        }
+                        // aggiorno l'adapter con i nuovi dati
+                        if (adapter != null) {
+                            adapter.clear();
+                            adapter.addAll(tesiList);
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                    return tesiList;
+                });
+    }
 }

94d92965eac21ba0c09df84846ced8c697e0a1be
BarbieriGiuseppe <giuseppebarbieri16@gmail.com>
2023-10-17 19:24:46 +0200

    Cambiamenti: Implementato il filtra per cognome del relatore.
    To-Do
    continuare implementazione del filtra

--- app/src/main/java/com/laureapp/ui/card/TesiStudente/ElencoTesiFragment.java
+++ app/src/main/java/com/laureapp/ui/card/TesiStudente/ElencoTesiFragment.java
@@ -2,32 +2,31 @@ package com.laureapp.ui.card.TesiStudente;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.widget.SearchView;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.laureapp.R;
 import com.laureapp.ui.card.Adapter.ElencoTesiAdapter;
 import com.laureapp.ui.roomdb.entity.Professore;
 import com.laureapp.ui.roomdb.entity.Tesi;
-import com.laureapp.ui.roomdb.entity.TesiProfessore;
+import com.laureapp.ui.roomdb.entity.Utente;
 import com.laureapp.ui.roomdb.viewModel.ProfessoreModelView;
-import com.laureapp.ui.roomdb.viewModel.TesiProfessoreModelView;
+import com.laureapp.ui.roomdb.viewModel.UtenteModelView;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -43,9 +42,10 @@ public class ElencoTesiFragment extends Fragment {
     private AlertDialog filterDialog;
     ArrayList<Tesi> tesiList = new ArrayList<>();
 
-    ArrayList<Long> idTesis = new ArrayList<>();
-    final ArrayList<Long> idTesiProfessoreList = new ArrayList<>();
+    List<Utente> utentiList = new ArrayList<>();
+    List<Professore> professoriList = new ArrayList<>();
 
+    ArrayList<Long> idTesiList = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -60,6 +60,14 @@ public class ElencoTesiFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         context = getContext();
 
+        //Ottengo la lista degli utenti che mi servirà nel filtra
+        UtenteModelView utenteModelView = new UtenteModelView(context);
+        utentiList = utenteModelView.getAllUtente();
+
+        //ottengo la lista dei professori che mi servirà nel filtra
+        ProfessoreModelView professoreModelView = new ProfessoreModelView(context);
+        professoriList = professoreModelView.getAllProfessore();
+
         SearchView searchView = view.findViewById(R.id.searchTesiView);
         listView = view.findViewById(R.id.listClassificaTesiView);
 
@@ -67,7 +75,7 @@ public class ElencoTesiFragment extends Fragment {
         Button filterButton = view.findViewById(R.id.filterButton);
         filterButton.setOnClickListener(view1 -> {
             view.setAlpha(0.1f); // Imposta l'opacità desiderata (0.0-1.0)
-            showFilterDialog(view,tesiList); //quando viene cliccato il pulsante filtra si apre il dialog
+            showFilterDialog(view,tesiList,utentiList); //quando viene cliccato il pulsante filtra si apre il dialog
         });
 
 
@@ -191,7 +199,7 @@ public class ElencoTesiFragment extends Fragment {
                 });
     }
 
-    public void showFilterDialog(View rootView, ArrayList<Tesi> tesiList) {
+    public void showFilterDialog(View rootView, ArrayList<Tesi> tesiList, List<Utente> utentiList) {
         AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
         View view = LayoutInflater.from(requireContext()).inflate(R.layout.fragment_popup_filtra_tesi, null);
         builder.setView(view);
@@ -219,9 +227,31 @@ public class ElencoTesiFragment extends Fragment {
             String media = editTextMedia.getText().toString();
             String esami = editTextEsami.getText().toString();
 
-            getTesiIds(); //chiamo il metodo per ottenere gli id delle tesi
-            loadIdTesiProfessoreData();
+           Long id_utente =  getIdUtenteByCognome(cognome);
+           if(id_utente != null) {
 
+               Long id_professore = getIdProfessorebyIdUtente(id_utente);
+                //chiamo il metodo per caricare gli id  delle tesi in base all'id del professore
+               loadIdTesiDataByProfessoreId(id_professore).addOnCompleteListener(task -> {
+                   if (task.isSuccessful()) { //se il task è completato con successo
+                       idTesiList = task.getResult(); //assegno gli id delle tesi ad una lista di tipo Long
+                       loadTesiDataByCognomeRelatore(idTesiList);
+                       filterDialog.dismiss();
+                   } else {
+                       Log.e("Firestore Error", "Error getting data", task.getException());
+                   }
+               });
+
+
+
+           }else{
+               filterDialog.dismiss();
+               Toast toast = Toast.makeText(context, "Nessun risultato", Toast.LENGTH_SHORT);
+               toast.setGravity(Gravity.TOP, 0, 0); // Imposta il Toast in alto
+               toast.show();
+           }
+
+           Log.d("cogn3", String.valueOf(id_utente));
 
         });
 
@@ -246,55 +276,115 @@ public class ElencoTesiFragment extends Fragment {
     }
 
 
+
     /**
-     * Metodo per ottenere gli id delle tesi dalla lista delle tesi
+     * Metodo per ottenere gli id degli utenti cercandoli in base al cognome
      * @return id delle tesi
      */
-    private ArrayList<Long> getTesiIds() {
-        for (Tesi tesi : tesiList) {
-            Long tesiId = tesi.getId_tesi();
-            idTesis.add(tesiId);
-        }
-        return idTesis;
+    private Long getIdUtenteByCognome(String cognome) {
 
+        Long id_utente = null;
+        for (Utente utente : utentiList) { //ciclo gli utenti
+
+            if (utente.getCognome().equalsIgnoreCase(cognome)) { //se il cognome dell'utente corrisponde
+                return id_utente = utente.getId_utente(); //ritorno il suo id
+
+            }
+
+        }
+        return id_utente;
     }
 
     /**
-     * Questo metodo mi permette di caricare da firestore gli id delle tesi dando come parametro l'id dello studente
-     *
-     *
-     * @return una lista di tipo Long contenente gli id delle tesi associate allo studente
+     * Metodo per ottenre l'id del professore, qualora lo sia, in base all'id utente
+     * @param id_utente
+     * @return id del professore
      */
-    private Task<ArrayList<Long>> loadIdTesiProfessoreData() {
-        final ArrayList<Task<QuerySnapshot>> queryTasks = new ArrayList<>();
-        final ArrayList<Long> idTesiProfessoreList = new ArrayList<>();
+    private Long getIdProfessorebyIdUtente(Long id_utente){
+        Long id_professore = null;
 
-        // Itera su ciascun ID nella lista
-        for (Long id_tesi : idTesis) {
-            Log.d("jek1", String.valueOf(id_tesi));
-            Task<QuerySnapshot> queryTask = FirebaseFirestore.getInstance()
-                    .collection("TesiProfessore")
-                    .whereEqualTo("id_tesi", id_tesi)
-                    .get();
-            queryTasks.add(queryTask);
+        for(Professore professore : professoriList){
+            if(professore.getId_utente().equals(id_utente)){
+                return id_professore = professore.getId_professore();
+            }
         }
 
-        return Tasks.whenAllSuccess(queryTasks)
-                .continueWith(task -> {
-                    for (Task<QuerySnapshot> queryResultTask : queryTasks) {
-                        if (queryResultTask.isSuccessful()) {
-                            QuerySnapshot querySnapshot = queryResultTask.getResult();
-                            for (QueryDocumentSnapshot doc : querySnapshot) {
-                                Long data = doc.getLong("id_tesi");
-                                if (data != null) {
-                                    idTesiProfessoreList.add(data);
-                                    Log.d("jek", String.valueOf(idTesiProfessoreList));
+        return id_professore;
+    }
 
-                                }
+
+    /**
+     * Questo metodo mi permette di caricare da firestore gli id delle tesi dando come parametro l'id del professore
+     *
+     * @param id_professore
+     * @return una lista di tipo Long contenente gli id delle tesi associate allo studente
+     */
+    private Task<ArrayList<Long>> loadIdTesiDataByProfessoreId(Long id_professore) {
+        final ArrayList<Long> idTesiList = new ArrayList<>();
+
+        return FirebaseFirestore.getInstance()
+                .collection("TesiProfessore")
+                .whereEqualTo("id_professore", id_professore)
+                .get()
+                .continueWith(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot doc : task.getResult()) {
+                            Long data = doc.getLong("id_tesi");
+                            if (data != null) {
+                                idTesiList.add(data);
                             }
                         }
                     }
-                    return idTesiProfessoreList;
+                    return idTesiList;
+                });
+    }
+
+    private Task<ArrayList<Tesi>> loadTesiDataByCognomeRelatore(ArrayList<Long> idTesiList) {
+        final ArrayList<Tesi> tesiList = new ArrayList<>();
+
+        return FirebaseFirestore.getInstance()
+                .collection("Tesi")
+                .get()
+                .continueWith(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Tesi tesi = new Tesi();
+                            tesi.setId_tesi((Long) document.get("id_tesi"));
+                            tesi.setId_vincolo((Long) document.get("id_vincolo"));
+
+                            // Converto da firebase timestamp a sql timestamp
+                            com.google.firebase.Timestamp firebaseTimestamp = (com.google.firebase.Timestamp) document.get("data_pubblicazione");
+                            Date javaDate = firebaseTimestamp.toDate();
+                            java.sql.Timestamp sqlTimestamp = new java.sql.Timestamp(javaDate.getTime());
+                            tesi.setData_pubblicazione(sqlTimestamp);
+
+                            tesi.setCiclo_cdl((String) document.get("ciclo_cdl"));
+                            tesi.setAbstract_tesi((String) document.get("abstract_tesi"));
+                            tesi.setTitolo((String) document.get("titolo"));
+                            tesi.setTipologia((String) document.get("tipologia"));
+
+                            tesiList.add(tesi);
+                        }
+
+                        // Itera attraverso gli idTesiList
+                        ArrayList<Tesi> tesiByCognomeRelatore = new ArrayList<>();
+                        for (Long idTesi : idTesiList) {
+                            for (Tesi tesi : tesiList) {
+                                if (idTesi.equals(tesi.getId_tesi())) {
+                                    tesiByCognomeRelatore.add(tesi);
+                                    break; // Esci dal ciclo interno dopo aver trovato una corrispondenza
+                                }
+                            }
+                        }
+
+                        // Aggiorna l'adapter con i nuovi dati
+                        if (adapter != null) {
+                            adapter.clear();
+                            adapter.addAll(tesiByCognomeRelatore);
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                    return tesiList;
                 });
     }
 

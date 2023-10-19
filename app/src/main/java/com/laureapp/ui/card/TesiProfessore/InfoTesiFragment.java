e9fc9b2bc9d773ef2d28cbd106092dc4b33eb3c9
Mario <supermario.br1@gmail.com>
2023-10-19 11:20:53 +0200

    Creato layout, da implementare, Vincoli, tesi lato professore

--- app/src/main/java/com/laureapp/ui/card/TesiProfessore/InfoTesiFragment.java
+++ app/src/main/java/com/laureapp/ui/card/TesiProfessore/InfoTesiFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 
 import com.laureapp.R;
 import com.laureapp.databinding.FragmentInfoTesiProfessoreBinding;
+import com.laureapp.databinding.FragmentProfiloBinding;
 import com.laureapp.ui.roomdb.entity.Tesi;
 import com.laureapp.ui.roomdb.viewModel.TesiModelView;
 
@@ -20,6 +22,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.StringJoiner;
 
 public class InfoTesiFragment extends Fragment {
 
@@ -38,32 +41,35 @@ public class InfoTesiFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle args) {
 
-        /*context = requireContext();
-        t_view = new TesiModelView(context);
-        args = getArguments();*/
+        context = requireContext();
+        //t_view = new TesiModelView(context);
+        Log.d("Tesi passata", String.valueOf(getArguments()));
+        args = getArguments();
+        if(args != null) {
+            tesi = args.getSerializable("Tesi",Tesi.class);
+        }
 
-        return inflater.inflate(R.layout.fragment_info_tesi_professore, container, false);
+        binding = FragmentInfoTesiProfessoreBinding.inflate(inflater, container, false);
+        return binding.getRoot();
 
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Log.d("qualcosa","qualcosa");
-        Bundle args = getArguments();
-        Log.d("Parametri", String.valueOf(args));
+        //Bundle args = getArguments();
+        //Log.d("Parametri", String.valueOf(args));
 
         /*binding.inserisciTitolo.setText(tesi.getTitolo());
         binding.inserisciTipologia.setText(tesi.getTipologia());
-        binding.inserisciDataPubblicazione.setText(tesi.getData_pubblicazione());
+        binding.inserisciDataPubblicazione.setText(String.valueOf(tesi.getData_pubblicazione())); //Converto la data in Stringa con il casting
         binding.inserisciCicloCdl.setText(tesi.getCiclo_cdl());
         binding.inserisciAbstract.setText(tesi.getAbstract_tesi());*/
 
+        /*TextView text = view.findViewById(R.id.inserisciTitolo);
+        text.setText("DCNHASKC");*/ //alternativa del binding
+
 
     }
-
-    /*private void set_view() {
-        binding.cardInfoTesi.setVisibility(View.GONE);
-    }*/
-
 
 }

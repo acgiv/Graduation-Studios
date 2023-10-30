88301c73ff6de3a4c4c9a96aea0ca8b9b95b468c
BarbieriGiuseppe <giuseppebarbieri16@gmail.com>
2023-10-30 09:05:01 +0100

    Cambiamenti: implementazione caricamento materiale tesi To-Do

--- app/src/main/java/com/laureapp/ui/card/TesiProfessore/ListaTesiProfessoreFragment.java
+++ app/src/main/java/com/laureapp/ui/card/TesiProfessore/ListaTesiProfessoreFragment.java
@@ -69,8 +69,6 @@ public class ListaTesiProfessoreFragment extends Fragment {
     List<Utente> utenteList = new ArrayList<>();
     List<String> nomeCognomeProfessoreList = new ArrayList<>();
 
-    Long professoreSceltoId;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_lista_tesi_professore, container, false);
---
+++ app/src/main/java/com/laureapp/ui/roomdb/dao/FileTesiDao.java
@@ -0,0 +1,33 @@
+package com.laureapp.ui.roomdb.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.laureapp.ui.roomdb.entity.FileTesi;
+import com.laureapp.ui.roomdb.entity.Tesi;
+
+import java.util.List;
+
+@Dao
+public interface FileTesiDao {
+
+    @Insert
+    void insert(FileTesi fileTesi);
+
+    @Update
+    void update(FileTesi fileTesi);
+
+    @Query("SELECT * FROM FileTesi")
+    List<FileTesi> getAllTesi();
+
+    @Query("SELECT * FROM FileTesi where id_file = :idFile")
+    Tesi findAllById(Long idFile);
+
+    @Delete
+    void delete(FileTesi fileTesi);
+
+
+}
